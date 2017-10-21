02dd603072fa92af3f5b94f8c4d6a59aba4c5c1e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-21 18:35:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 
 def speak_to_grandma(str)
   if (str == "I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif (str == str.upcase)
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
