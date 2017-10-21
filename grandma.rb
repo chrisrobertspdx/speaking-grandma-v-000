29389f37401c5eec328fcd280ee24a95965f9998
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-21 18:32:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(str)
   if (str == "I LOVE YOU GRANDMA!")
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif (str == str.upper)
+  elsif (str == str.upper())
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
